fad8f5f66e51dbd0bc8eaf7109248249a2a1ad54
Kristian Kubis <kubis98.k@gmail.com>
2019-02-19 16:34:42 +0100

    Added paralax scrolling + put monikas part in

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/*
+const paralax = document.getElementById("experience_h");
+
+window.addEventListener("scroll", function(){
+    let offset = window.pageYOffset;
+    /*console.log(offset);  
+    paralax.style.backgroundPositionY = offset*0.2 + "px";
+    
+})
+
+*/
+
+
+
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
